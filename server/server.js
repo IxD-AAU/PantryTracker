c6e0f59632b84d670857ca1b1f62f268032b1b97
Casp-AAU-dev <cbojen22@student.aau.dk>
2025-11-11 10:38:12 +0100

    updated server file to work with both express and mysql. database is now online

--- server/server.js
+++ server/server.js
@@ -1,22 +1,51 @@
+const mysql = require('mysql2');
+
 const express = require('express');
 
-
 const app = express();
+const PORT = 3000;
 
+app.use(express.json());
 
-
-const cors = require('cors');
-
-app.use(cors(origin = "http://localhost:4200"));
-
-
-app.use(express.static('<path to your angular app>'));
-
-
-app.get('/api/hello', (req, res) => {
-    res.send({ message: 'Hello World!'});
-   });
-
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000!');
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "DBuser",
+    password: "DBpassword",
+    database: "sys"
 });
+
+connection.connect(function(err){
+    if (err){
+        console.error ('DB connect error: ' + err.code, err.message);
+        process.exit(1);
+    }
+    console.log('DB connected as id ' + connection.threadId);
+});
+
+app.listen(PORT,() => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+})
+
+app.get('/', (req, res) => {
+    res.send('Welcome to Pantry Tracker API');
+});
+
+app.get('/api/data', (req, res) => {
+    connection.query('SELECT * FROM usertable', (err, results) => {
+        if (err) {
+            console.error(err);
+            res.status(500).json({ error: 'Database query failed' });
+            return;
+        }
+        res.json(results);
+    });
+});
+
+
+
+process.on('SIGINT', ()=>{
+    connection.end();
+    process.exit(0);
+})
+
+
