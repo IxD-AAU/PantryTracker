d27e15818832f141cd3e009529183dc630fb8bc5
EmmaBChr <Emmablom@live.dk>
2025-11-17 16:56:09 +0100

    Routing virker
    Skiftet til RouterLink

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { PhoneSquareComponent } from './phone-square/phone-square.component';
-import { HomeButComponent } from './phone-square/nav-bar/home-but/home-but.component';
-import { GroceryListButComponent } from './phone-square/nav-bar/grocery-list-but/grocery-list-but.component';
-import { CabinetsButComponent } from './phone-square/nav-bar/cabinets-but/cabinets-but.component';
-import { HouseholdButComponent } from './phone-square/nav-bar/household-but/household-but.component';
-import { RecipeButComponent } from './phone-square/nav-bar/recipe-but/recipe-but.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { GroceryListPageComponent } from './pages/grocery-list-page/grocery-list-page.component';
+import { CabinetsPageComponent } from './pages/cabinets-page/cabinets-page.component';
+import { HouseholdPageComponent } from './pages/household-page/household-page.component';
+import { RecipePageComponent } from './pages/recipe-page/recipe-page.component';
 
 export const appRoutes: Routes = [
-  { path: '', component: PhoneSquareComponent,
+  { 
+    path: '', 
+    component: PhoneSquareComponent,
     children: [
-  {path: "home-but", redirectTo: "home-page", pathMatch: "full"},
-  {path: "grocery-list-but", redirectTo: "grocery-list-page", pathMatch: "full"},
-  {path: "cabinets-but", redirectTo: "cabinets-page", pathMatch: "full"}, 
-  {path: "household-but", redirectTo: "household-page", pathMatch: "full"}, 
-  {path: "recipe-but", redirectTo: "recipe-page", pathMatch: "full"},
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'home', component: HomePageComponent },
+      { path: 'grocery-list', component: GroceryListPageComponent },
+      { path: 'cabinets', component: CabinetsPageComponent },
+      { path: 'household', component: HouseholdPageComponent },
+      { path: 'recipe', component: RecipePageComponent }
     ]
   }
 ];
