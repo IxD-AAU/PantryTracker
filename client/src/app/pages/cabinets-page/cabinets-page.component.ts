59d123e1934a96e4f7ae82aa1a0385442c933e5c
EmmaBChr <Emmablom@live.dk>
2025-11-18 18:28:25 +0100

    skabe ind på skabe
    har prøvet at sætte nogle skabe ind på skabs siden

--- client/src/app/pages/cabinets-page/cabinets-page.component.ts
+++ client/src/app/pages/cabinets-page/cabinets-page.component.ts
@@ -1,16 +1,20 @@
 import { Component } from '@angular/core';
 import { PageTitleComponent } from '../../shared/page-title/page-title.component';
 import { AddButtonComponent } from '../../shared/add-button/add-button.component';
+import { CabinetNormalComponent } from '../../shared/cabinet-normal/cabinet-normal.component';
 
 @Component({
   selector: 'app-cabinets-page',
   standalone: true,
-  imports: [PageTitleComponent, AddButtonComponent],
+  imports: [PageTitleComponent, AddButtonComponent, CabinetNormalComponent],
   templateUrl: './cabinets-page.component.html',
   styleUrl: './cabinets-page.component.css'
 })
 export class CabinetsPageComponent {
  onAddClick() {
     console.log('Add button clicked!');
+  }
+   onCabinetClick() {
+    console.log('Cabinet button clicked!');
   }
 }
