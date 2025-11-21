ea858c6672ca3ce11823099578f8dbf68ef6ef12
Aschou84 <aschou24@student.aau.dk>
2025-11-21 14:51:38 +0100

    Scanner button added for aditems pages, in the right position but text is not set right yet

--- client/src/app/pages/add-item-page/add-item-page.component.ts
+++ client/src/app/pages/add-item-page/add-item-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { PageTitleComponent } from '../../shared/page-title/page-title.component';
 import { RouterModule } from '@angular/router';
+import { ScannerButtonComponent } from './scanner-button/scanner-button.component';
 
 @Component({
   selector: 'app-add-item-page',
   standalone: true,
-  imports: [PageTitleComponent, RouterModule],
+  imports: [PageTitleComponent, ScannerButtonComponent, RouterModule],
   templateUrl: './add-item-page.component.html',
   styleUrl: './add-item-page.component.css'
 })
---
+++ client/src/app/pages/add-item-page/scanner-button/scanner-button.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ScannerButtonComponent } from './scanner-button.component';
+
+describe('ScannerButtonComponent', () => {
+  let component: ScannerButtonComponent;
+  let fixture: ComponentFixture<ScannerButtonComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ScannerButtonComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ScannerButtonComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
