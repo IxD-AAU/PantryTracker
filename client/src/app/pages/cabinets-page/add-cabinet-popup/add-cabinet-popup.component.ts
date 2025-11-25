573bcc6daac30a6f12669cf4eec4586d042a0c35
EmmaBChr <Emmablom@live.dk>
2025-11-25 09:52:25 +0100

    Input felt i popup

--- client/src/app/pages/cabinets-page/add-cabinet-popup/add-cabinet-popup.component.ts
+++ client/src/app/pages/cabinets-page/add-cabinet-popup/add-cabinet-popup.component.ts
@@ -1,15 +1,17 @@
 import { Component, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-cabinet-popup',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './add-cabinet-popup.component.html',
   styleUrl: './add-cabinet-popup.component.css'
 })
 export class AddCabinetPopupComponent {
   @Output() closed = new EventEmitter<void>();
+  cabinetName = '';
 
   onClose() {
     this.closed.emit();
