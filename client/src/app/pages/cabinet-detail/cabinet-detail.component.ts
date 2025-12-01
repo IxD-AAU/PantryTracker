8b20199c2f68a3d3c9b5d507f2d77bc028222979
EmmaBChr <Emmablom@live.dk>
2025-12-01 18:53:21 +0100

    Title på skabe underside

--- client/src/app/pages/cabinet-detail/cabinet-detail.component.ts
+++ client/src/app/pages/cabinet-detail/cabinet-detail.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CabinetService } from '../../services/cabinet.service';
+import { PageTitleComponent } from '../../shared/page-title/page-title.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cabinet-detail',
-  imports: [],
+  standalone: true,
+  imports: [PageTitleComponent, CommonModule],
   templateUrl: './cabinet-detail.component.html',
   styleUrl: './cabinet-detail.component.css'
 })
-export class CabinetDetailComponent {
+export class CabinetDetailComponent implements OnInit {
+  cabinetName: string = '';
+  cabinetIndex: number = 0;
 
+  constructor(
+    private route: ActivatedRoute,
+    private cabinetService: CabinetService
+  ) {}
+
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.cabinetIndex = +params['id'];
+      const cabinets = this.cabinetService.getCabinets();
+      if (cabinets[this.cabinetIndex]) {
+        this.cabinetName = cabinets[this.cabinetIndex].name;
+      }
+    });
+  }
 }
