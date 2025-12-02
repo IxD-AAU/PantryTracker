257e37b35cf30b207f1ab1109366af9344349bf0
Aschou84 <aschou24@student.aau.dk>
2025-12-02 13:03:28 +0100

    add items page: somethings has been added

---
+++ client/src/app/shared/change-amount/change-amount.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { AddButtonComponent } from '../add-button/add-button.component';
+
+@Component({
+  selector: 'app-change-amount',
+  imports: [AddButtonComponent],
+  templateUrl: './change-amount.component.html',
+  styleUrl: './change-amount.component.css'
+})
+export class ChangeAmountComponent {
+
+}
--- client/src/app/shared/small-green-line/small-green-line.component.ts
+++ client/src/app/shared/small-green-line/small-green-line.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-small-green-line',
+  standalone: true,
   imports: [],
   templateUrl: './small-green-line.component.html',
   styleUrl: './small-green-line.component.css'
