2585b8eb9fa40bf9737f0805fb6c318aa9b14355
EmmaBChr <Emmablom@live.dk>
2025-12-02 09:11:43 +0100

    Har glemt hvad det her er

--- client/src/app/pages/grocery-list-page/grocery-list-page.component.ts
+++ client/src/app/pages/grocery-list-page/grocery-list-page.component.ts
@@ -1,23 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PageTitleComponent } from '../../shared/page-title/page-title.component';
 import { AddButtonComponent } from '../../shared/add-button/add-button.component';
 import { Router } from '@angular/router';
 import { AddListButtonComponent } from '../../shared/add-list-button/add-list-button.component';
 import { AddListPopupComponent } from './add-list-popup/add-list-popup.component';
+import { ListComponent } from '../../shared/list/list.component';
+import { ListService } from '../../services/list.service';
 
 
 @Component({
   selector: 'app-grocery-list-page',
-  imports: [CommonModule, PageTitleComponent, AddButtonComponent, AddListButtonComponent, AddListPopupComponent],
+  imports: [CommonModule, PageTitleComponent, AddButtonComponent, AddListButtonComponent, AddListPopupComponent, ListComponent],
   standalone: true,
   templateUrl: './grocery-list-page.component.html',
   styleUrl: './grocery-list-page.component.css'
 })
-export class GroceryListPageComponent {
+export class GroceryListPageComponent implements OnInit {
   showAddListPopup = false;
+  userLists: any[] = [];
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private listService: ListService) {}
+
+  ngOnInit() {
+    this.userLists = this.listService.getLists();
+  }
 
   onAddClick() {
     this.router.navigate(['/add-item']);
@@ -32,7 +39,8 @@ export class GroceryListPageComponent {
   }
 
   onListAdded(listName: string) {
-    console.log('List added:', listName);
+    this.listService.addList({ name: listName });
+    this.userLists = this.listService.getLists();
     this.showAddListPopup = false;
   }
 }
